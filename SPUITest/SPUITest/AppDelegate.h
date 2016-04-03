b4f4380a0ddb2622185dec08f51407313ab8b08e
GodLikeEcho <victosme@gmail.com>
2016-04-03 10:45:05 -0700

    Initial project
    started a project

---
+++ SPUITest/SPUITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPUITest
+//
+//  Created by Clint Jellesed on 4/3/16.
+//  Copyright © 2016 Clint Jellesed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SPUITest/SPUITest/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SPUITest
+//
+//  Created by Clint Jellesed on 4/3/16.
+//  Copyright © 2016 Clint Jellesed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
