e317d46f733ab7a5880141a33ad20a44116a41c6
GodLikeEcho <victosme@gmail.com>
2017-03-01 01:39:12 -0800

    UserHP
    User page and custom table cells!!

---
+++ FCTracker/FCTracker/CustomTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  CustomTableViewCell.swift
+//  FCTracker
+//
+//  Created by Clint Jellesed on 2/28/17.
+//  Copyright © 2017 Clint Jellesed. All rights reserved.
+//
+
+//import Cocoa
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var fcName: UILabel!
+    @IBOutlet var fcLocation: UILabel!
+    @IBOutlet var fcHours: UILabel!
+    @IBOutlet var fcRate: UILabel!
+    
+    let lightGreen = UIColor(red: 0.5, green: 1.0, blue: 0.5, alpha: 1.0)
+    let lightRed = UIColor(red: 1.0, green: 0.9, blue: 0.9, alpha: 1.0)
+    
+    //let discount = 0.1
+    
+//    func string(_ prefix:String, for price:Double) -> String{
+//        let priceString = String(format: "%2.2f", price)
+//        return prefix + priceString
+//    }
+    
+    func set(Name:String, Locaton:String, Hours:String, Rate:String){
+        
+            fcName.text = Name
+            fcLocation.text = Locaton
+            fcHours.text = Hours
+            fcRate.text = Rate
+            contentView.backgroundColor = lightRed
+    }
+}
--- FCTracker/FCTracker/UHomeViewController.swift
+++ FCTracker/FCTracker/UHomeViewController.swift
@@ -9,11 +9,19 @@
 //import Foundation
 import UIKit
 
-class UHomeViewController: UIViewController {
+class UHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.contentSize = CGSizeMake(self.view.frame.width, self.view.frame.height+1000)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 75
+        self.tableView.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,5 +29,46 @@ class UHomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2//items.count
+    }
+    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+//        
+//        //let item = items[indexPath.row]
+//        //cell.textLabel?.text = "Item: \(items[indexPath.row])"
+//        //cell.textLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
+//        //cell.textLabel?.numberOfLines = 0;
+//        
+//        return cell
+//    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:CustomTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("customCell", forIndexPath: indexPath) as! CustomTableViewCell
+        //let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! CustomTableViewCell
+        //let row = indexPath.row
+        //print(cell2)
+        cell.set("Test", Locaton: "temp", Hours: "trrr", Rate: "yolo")
+        return cell
+    }
+    
+//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        var returnValue = CGFloat()
+//        
+//        returnValue = CGFloat(85.0);
+//        return returnValue;
+//    }
+//    
+//    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        return UITableViewAutomaticDimension
+//    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
     
 }
